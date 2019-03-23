ec61138bfae14394d74aebbcc220c57070fde801
fedodd <fedodd.s@gmail.com>
2019-03-23 11:45:32 +0300

    start project

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+
+class App extends Component {
+
+  state = {
+  };
+  
+  render() {
+    return (
+      <div className={classes.App}>
+        <h1>Hello, world!</h1>
+      </div>
+     );
+  }
+};
+
+export default App;
