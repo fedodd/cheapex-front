eea8b9c4780989a947749519b2ab1bf3e66192dc
fedodd <fedodd.s@gmail.com>
2019-03-26 18:06:14 +0300

    titleHandler

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
 import classes from './App.pcss';
+import declOfNum from "../functions/declOfNum";
 
 class App extends Component {
 
   state = {
-
+    totalItems: 15,
   };
+
+  titleEnding = declOfNum(this.state.totalItems,  ['компании', 'компаний', 'компаний']);
 
   render () {
     return (
       <div>
-        
+        <h1>Лучшие предложения по вашему запросу от {this.state.totalItems} {this.titleEnding}</h1>
       </div>
     );
   }
