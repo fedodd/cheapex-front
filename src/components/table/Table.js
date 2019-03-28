91ab52667549d863dfac87dc2143931c52b97563
fedodd <fedodd.s@gmail.com>
2019-03-28 14:17:16 +0300

    ask data from server

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import Header from './Header/Header';
-import Row from './Row/Row';
-import './Table.pcss';
+import React, { Component } from 'react';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 
-const table = (props) => {
-  return (
-    <div className="table">
 
-    </div>
-  );
+//import Header from './Header/Header';
+//import Row from './Row/Row';
+import classes from './Table.pcss';
+
+
+class Table extends Component {
+
+  state = {
+    companies: [],
+    tablerows: []
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:4000/').then(res => res.json()).then(res => this.setState({companies: res}));
+    fetch('http://localhost:4000/').then(res => res.json()).then(res => this.setState({ tablerows: res }));
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.state.tablerows.map((row, index) => {
+          return (
+            <div className={classes.row}>{
+              Object.keys(row).map(key => {
+                if (key !== '_id' && key !== '__v') {
+                  return <span className={classes.cell}>{row[key]}</span>
+                }
+                return null;
+              })
+            }</div>
+          )
+        })}
+      </div>
+     );
+  }
 };
 
-export default table;
+export default Table;
