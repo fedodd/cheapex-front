6ee48c16a2805ca7e5e0430841ae2f00395ad3f0
fedodd <fedodd.s@gmail.com>
2019-06-03 00:27:56 +0300

    transcript styling

--- src/containers/headerHelpers/transcriptHanlder.js
+++ src/containers/headerHelpers/transcriptHanlder.js
@@ -4,7 +4,8 @@ const transcriptHandler = (data, targetColumns) => {
   data.map(row => {
     targetColumns.map((transcriptIndex) => {
       if (row[transcriptIndex] !== null) {
-        row[transcriptIndex - 1] = <span className="transcriptWrapper">{row[transcriptIndex - 1]}<span className="transcripted">{row[transcriptIndex]}</span></span>
+        
+        row[transcriptIndex - 1] = <span className="transcriptWrapper"><span className="transcripted">{row[transcriptIndex]}</span>{row[transcriptIndex - 1]}</span>
         return null;
         //если расшифровки нет, все равно вешаем класс, т.к. мы чекаем размер колонки по названию класса колонки
       } else {
